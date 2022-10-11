c927c44a22f395b6cfc0ce46cb1dabf44c1fa6fc
Aaron L <aaron.lazaroo@etu.umontpellier.fr>
2022-10-11 10:18:37 +0200

    Fetching data from api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,41 @@
-function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
+import React from "react";
+import './App.css';
+import {useEffect} from "react";
+
+class App extends React.Component{
+
+  // Constructor 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        items: [],
+        DataisLoaded: false
+    };
 }
+// fetching data from api
+    componentDidMount() {
+      fetch(
+"https://localhost:3000/api/posts")
+          .then((res) => res.json())
+          .then((json) => {
+              this.setState({
+                  items: json,
+                  DataisLoaded: true
+              });
+          })
+  }
+
+render() {
+  const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Please wait some time.... </h1> </div> ;
+        console.log(items);
+return <div>{
+
+    }</div>;
+}
+}
+  
 
 export default App;
