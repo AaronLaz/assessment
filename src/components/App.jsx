c1ab1be1f248bbe1aae532afb4e649c908622d19
Aaron L <aaron.lazaroo@etu.umontpellier.fr>
2022-10-11 11:51:36 +0200

    Add rudimentary list display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import {useEffect} from "react";
+import dateFormat from 'dateformat';
 
 class App extends React.Component{
 
@@ -31,9 +31,41 @@ render() {
         if (!DataisLoaded) return <div>
             <h1> Please wait some time.... </h1> </div> ;
         console.log(items);
-return <div>{
-
-    }</div>;
+return (
+  <div className = "App">
+            {/* Rudimentary display, todo: add component */}
+            <h1> Lizard Global Assessment - Posts </h1>  {
+                items.posts.map((item) => ( 
+                <ol key = { item.id } >
+                    <pre>
+                      Title: { item.title }
+                    </pre>
+                    <pre>
+                      Author: { item.author.name } 
+                    </pre>
+                    <pre>
+                      {/* Date format to improve readability */}
+                      Publish date: { dateFormat(item.publishDate, "mmmm dS yyyy") } 
+                    </pre>      
+                    <pre>
+                      {/* Display categories in a list */}
+                      Categories: { 
+                    item.categories.map((category) => (
+                      <ul key = { category.id } >
+                        {category.name}
+                        </ul>
+                      ))
+                    } 
+                    </pre>
+                    {/* Summary might not be necessary in the list, might add to detail page */}
+                    {/* <pre>
+                      Summary: { item.summary }
+                    </pre>               */}
+                    </ol>
+                ))
+            }
+        </div>
+);
 }
 }
   
